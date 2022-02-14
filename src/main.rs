e9bcd209e6f206fee12374e327cd774f4e5c47bb
Otavio Salvador <otavio@ossystems.com.br>
2022-02-14 10:26:01 -0300

    Move from structopt to clap
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@
 // SPDX-License-Identifier: MIT
 
 use crate::{amqp::AMQP, config::Config, sendgrid::SendGrid};
+use clap::Parser;
 use slog::info;
 use std::path::PathBuf;
-use structopt::StructOpt;
 
 mod amqp;
 mod build_info;
@@ -14,29 +14,28 @@ mod log;
 mod payload;
 mod sendgrid;
 
-#[structopt(
-    no_version,
+#[derive(Parser, Debug)]
+#[clap(
     name = "sendgrid-amqp-bridge",
     author = "O.S. Systems Software LTDA. <contact@ossystems.com.br>",
     about = "A SendGrid AMQP Bridge.",
     version = build_info::version()
 )]
-#[derive(StructOpt, Debug)]
 struct Cli {
     /// Configuration file to use
-    #[structopt(short = "c", long = "config")]
+    #[clap(short = 'c', long)]
     config: PathBuf,
     /// Increase the verboseness level
-    #[structopt(short = "v", long = "verbose", parse(from_occurrences))]
+    #[clap(short = 'v', long, parse(from_occurrences))]
     verbose: usize,
     /// Log output to use ('human' or 'json')
-    #[structopt(short = "l", long = "log", default_value = "human")]
+    #[clap(short = 'l', long, default_value = "human")]
     log: log::Output,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    let cli = Cli::from_args();
+    let cli = Cli::parse();
     let logger = log::init(cli.verbose, cli.log);
 
     info!(logger, "starting"; "version" => build_info::version());
